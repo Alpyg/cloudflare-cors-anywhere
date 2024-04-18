6633422905cc402765ce23e36b22ce6bb778dc04
Alpyg <alpyg@pm.me>
2024-04-18 18:09:57 -0400

    Cors anywhere worker

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,34 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const blacklist: string[] = [];
+const whitelist: string[] = ['.*'];
+
+const isListed = (uri: string, listing: string[]) => {
+	return listing.some((m) => uri.match(m));
+};
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const fetchUrl = request.url.split('?')[1] ?? '';
+		if (!fetchUrl) return new Response('https://cloudflare-cors-anywhere.alpyg.workers.dev/?<url>');
+		if (isListed(fetchUrl, blacklist) || !isListed(fetchUrl, whitelist)) return new Response('Poopoo');
+
+		const res = await fetch(fetchUrl);
+		const headers = new Headers(res.headers);
+
+		let cors_headers = [];
+		let allh: Record<string, string> = {};
+
+		for (const [key, value] of res.headers.entries()) {
+			cors_headers.push(key);
+			allh[key] = value;
+		}
+
+		cors_headers.push('cors-received-headers');
+		headers.set('Access-Control-Allow-Origin', request.headers.get('Origin') ?? '');
+
+		const data = await res.text();
+		return new Response(data, { headers: headers });
 	},
 };
